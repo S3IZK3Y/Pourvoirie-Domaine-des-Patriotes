5779017a68e473a5e540f580960b5eac480e6435
S3IZK3Y <maxim.deslandes@hotmail.ca>
2024-04-07 18:14:48 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+/* Photo en vedette du Barette*/
+const baretteVedette = document.querySelectorAll('.barette-vedette');
+const barette1 = "/Chalets/Barette/Photos/Barette1.jpg"
+baretteVedette.forEach((element) =>{
+    element.src = barette1
+});
+
+/* Photo en vedette du Bouchard */
+const bouchardVedette = document.querySelectorAll('.bouchard-vedette');
+const bouchard1 = "/Chalets/Bouchard/Photos/Bouchard1.jpg"
+bouchardVedette.forEach((element) =>{
+    element.src = bouchard1
+});
+
+/* Photo en vedette du GrandHuard */
+const grandHuardVedette = document.querySelectorAll('.grandhuard-vedette');
+const grandHuard1 = "/Chalets/GrandHuard/Photos/GrandHuard1.jpg";
+grandHuardVedette.forEach((element) => {
+    element.src = grandHuard1;
+});
+
+/* Photo en vedette du MaitreCamp */
+const maitreCampVedette = document.querySelectorAll('.maitrecamp-vedette');
+const maitreCamp1 = "/Chalets/MaitreCamp/Photos/MaitreCamp1.jpg";
+maitreCampVedette.forEach((element) => {
+    element.src = maitreCamp1;
+});
+
+/* Photo en vedette du Montredon */
+const montredonVedette = document.querySelectorAll('.montredon-vedette');
+const montredon1 = "/Chalets/Montredon/Photos/Montredon1.jpg";
+montredonVedette.forEach((element) => {
+    element.src = montredon1;
+});
+
+/* Photo en vedette du Myrtille */
+const myrtilleVedette = document.querySelectorAll('.myrtille-vedette');
+const myrtille1 = "/Chalets/Myrtille/Photos/Myrtille1.jpg";
+myrtilleVedette.forEach((element) => {
+    element.src = myrtille1;
+});
+
+/* Photo en vedette du PetitHuard */
+const petitHuardVedette = document.querySelectorAll('.petithuard-vedette');
+const petitHuard1 = "/Chalets/PetitHuard/Photos/PetitHuard1.jpg";
+petitHuardVedette.forEach((element) => {
+    element.src = petitHuard1;
+});
+
+/* Photo en vedette du Sly */
+const slyVedette = document.querySelectorAll('.sly-vedette');
+const sly1 = "/Chalets/Sly/Photos/Sly1.jpg";
+slyVedette.forEach((element) => {
+    element.src = sly1;
+});
+
